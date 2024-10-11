1f2ff9637a973bd8532ef138bfc89b368c5c93c4
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-10-11 18:53:28 +0530

    applied logger middleware

--- app/store.js
+++ app/store.js
@@ -1,13 +1,15 @@
 const configureStore = require('@reduxjs/toolkit').configureStore;
 const cakeReducer = require('../feature/cakeSlice');
 const icereamReducer = require('../feature/icecreamSlice');
+const logger = require('redux-logger').createLogger();
 
 
 const store = configureStore({
     reducer:{
         cake: cakeReducer,
         icecream: icereamReducer
-    }
+    },
+    middleware: (defaultMiddleware) => defaultMiddleware().concat(logger)
 })
 
 module.exports = store
